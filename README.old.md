14b065b5325a94ad0791ee2afe183e942950bb61
shhabib <shbulbul@gmail.com>
2025-10-07 17:28:24 +0600

    feat: Initialize React application with Create React App

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# BookTrack Application
+
+This project is a book collection management app for PageTurn Publishing, built with React.
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>BookTrack</h1>
+    </div>
+  );
+}
+export default App;
