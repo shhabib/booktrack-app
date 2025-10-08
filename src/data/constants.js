0893311c35824306e66eaa91e546eb700cf203a4
shhabib <shbulbul@gmail.com>
2025-10-08 11:48:33 +0600

    feat: Add initial mock data and status constants.

---
+++ src/data/constants.js
@@ -0,0 +1,5 @@
+export const STATUSES = {
+  wantToRead: { id: "wantToRead", label: "Want to Read" },
+  reading: { id: "reading", label: "Reading" },
+  finished: { id: "finished", label: "Finished" },
+};
